cccdf63aec535594b4e17f20c4a7bf4260d72e41
Jason Smith <js.jrex91@gmail.com>
2019-03-18 19:35:04 -0600

    Firebase info

---
+++ assets/javaScript/app.js
@@ -0,0 +1,9 @@
+ var config = {
+     apiKey: "AIzaSyByWnf1tK9rbRV4BmnDsPJiT_jzJkAmmU0",
+     authDomain: "edmwednesday-c32db.firebaseapp.com",
+     databaseURL: "https://edmwednesday-c32db.firebaseio.com",
+     projectId: "edmwednesday-c32db",
+     storageBucket: "edmwednesday-c32db.appspot.com",
+     messagingSenderId: "1070293597475"
+ };
+ firebase.initializeApp(config);
